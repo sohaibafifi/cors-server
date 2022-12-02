72f87617135ae3ba727b45bef4f982f9f32efb00
Sohaib Afifi <sohaibafifi@users.noreply.github.com>
2022-12-02 09:56:39 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,8 @@ const corsServer = corsAnywhere.createServer({
       'http://localhost:3000',
       'http://localhost:5000',
       'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com'
+      'https://portfolio-ravenous.firebaseapp.com',
+      'http://vtiutb.univ-artois.fr'
     ],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
